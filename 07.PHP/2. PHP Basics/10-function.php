216e81bbc4103e80a13860de40253506e0ef62af
Prasundas99 <prasundas115@gmail.com>
2021-04-17 21:20:09 +0530

    PHP basics added

---
+++ 07.PHP/2. PHP Basics/10-function.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function writeMsg() {
+    echo "Hello world!";
+}
+writeMsg();
+
+
+function familyName($fname) {
+    echo "$fname Refsnes.<br>";
+}
+
+familyName("Jani");
+familyName("Hege");
+familyName("Stale");
+familyName("Kai Jim");
+familyName("Borge");
+familyName(10000);
+?>
+
+</body>
+</html>
