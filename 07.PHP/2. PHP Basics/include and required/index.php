84382303281acf97a7b66dd9ce8f95f0f0d082a0
Prasundas99 <prasundas115@gmail.com>
2021-04-18 00:14:31 +0530

    include added

---
+++ 07.PHP/2. PHP Basics/include and required/index.php
@@ -0,0 +1,26 @@
+<?php 
+
+	//include('demos.php');
+	
+	//require('demos.php');
+
+	//include 'demo.php';
+
+	//require 'demo.php';
+
+	//echo 'end of php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Tutorials</title>
+</head>
+<body>
+
+	<?php include('content.php'); ?>
+	<?php include('content.php'); ?>
+
+</body>
+</html>
