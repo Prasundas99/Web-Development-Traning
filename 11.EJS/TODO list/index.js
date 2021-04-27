43fc907ca8fa0572a8ca19cf9e5e8ab2e11b2afd
Prasundas99 <prasundas115@gmail.com>
2021-04-27 20:28:19 +0530

    added todos

--- 11.EJS/TODO list/index.js
+++ 11.EJS/TODO list/index.js
@@ -18,5 +18,4 @@ console.log("Server started at port 3000");
 
 
 //MVC structure: https://stackoverflow.com/questions/3623172/why-is-mvc-so-popular#:~:text=In%20MVC%2C%20the%20Controller%20and,0%20dependencies%20on%20the%20View.
-
 // alternative to it : https://blog.ircmaxell.com/2014/11/alternatives-to-mvc.html
