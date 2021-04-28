d9e58b9740f8371686d87d5482578d812d05e2e6
Prasundas99 <prasundas115@gmail.com>
2021-04-28 17:16:12 +0530

    Project added

---
+++ 13. Project (EJS REST)/models/posts.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Postschema = mongoose.Schema({
+    Title: {
+        type: String,
+        required: true
+    },
+    Message: {
+        type: String,
+        required: true
+    },
+
+   
+
+});
+
+
+
+const Postmodel = mongoose.model("Postmodel", Postschema);
+
+module.exports = Postmodel;
