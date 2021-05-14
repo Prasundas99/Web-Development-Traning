266fd544e4acb56f295d08fb3c561053e3839514
Prasundas99 <prasundas115@gmail.com>
2021-05-14 23:18:43 +0530

    React ppt done

---
+++ 14.React/Image compressor/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import Compressor from "./Components/Compressor";
+
+function App() {
+  return (
+    <Compressor />
+  );
+}
+
+export default App;
