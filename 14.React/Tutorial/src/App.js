c345651c87d5dfff71dddc2b0183fa8de19a211e
Prasundas99 <prasundas115@gmail.com>
2021-06-08 20:00:33 +0530

    Finalised

--- 14.React/Tutorial/src/App.js
+++ 14.React/Tutorial/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import Main from './component/Main';
+import React from "react";
+import Main from "./component/Main";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
 function App() {
-	return (
-		<div>
-			<Main />
-		</div>
-	);
+  return (
+    <div>
+      <Main />
+{/*
+      <Router>
+        <Switch>
+		<Route exact path="/" component={} />
+		</Switch>
+      </Router>
+*/}
+
+    </div>
+  );
 }
 
 export default App;
--- 17.Angular/github-finder-angular/src/app/github.service.ts
+++ 17.Angular/github-finder-angular/src/app/github.service.ts
@@ -6,14 +6,14 @@ import 'rxjs';
 @Injectable()
 export class GithubService {
 
-  private username: string = "nihadkeric";
+  private username: string = "prasundas99";
   private client_id = '817eae476fcd5035749c';
   private client_secret = '7d1d4659a2eb8cfe692ec5e5c42a7823bee1a59b';
   
 
   constructor(private _http: Http) {
     console.log("Github service ready ...");
-    this.username = 'nihadkeric';
+    this.username = 'prasundas99';
   }
 
   getUser(){
