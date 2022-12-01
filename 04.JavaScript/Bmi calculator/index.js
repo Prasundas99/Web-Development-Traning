878743f45a5e731070009527724fa26a605e0c7b
Prasundas99 <prasundas115@gmail.com>
2022-12-02 00:04:25 +0530

    fix bmi calculator bug

--- 04.JavaScript/Bmi calculator/index.js
+++ 04.JavaScript/Bmi calculator/index.js
@@ -1,20 +1,24 @@
 function calculateBmi() {
-    var weight = document.bmiForm.weight.value
-    var height = document.bmiForm.height.value
-    if(weight > 0 && height > 0){	
-    var finalBmi = weight/(height/100*height/100)
-    document.bmiForm.bmi.value = finalBmi
-    if(finalBmi < 18.5){
-    document.bmiForm.meaning.value = "That you are too thin."
+  var weight = document.bmiForm.weight.value;
+  var height = document.bmiForm.height.value;
+
+
+  if (weight > 0 && height > 0) {
+    var finalBmi = weight / (((height / 100) * height) / 100);
+        document.bmiForm.bmi.value = finalBmi;
+    if (finalBmi < 18.5) {
+      document.bmiForm.meaning.value = "That you are too thin.";
     }
-    if(finalBmi > 18.5 && finalBmi < 25){
-    document.bmiForm.meaning.value = "That you are healthy."
+    if (finalBmi > 18.5 && finalBmi < 25) {
+      document.bmiForm.meaning.value = "That you are healthy.";
     }
-    if(finalBmi > 25){
-    document.bmiForm.meaning.value = "That you have overweight."
+    if (finalBmi > 25) {
+      document.bmiForm.meaning.value = "That you have overweight.";
     }
-    }
-    else{
-    alert("Please Fill in everything correctly")
-    }
-    }
+
+  } else {
+    alert("Please Fill in everything correctly");
+  }
+}
+
+
